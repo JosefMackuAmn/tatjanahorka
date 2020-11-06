c04ea43700019d6193752a5ee9611214ab5603f4
Josef Macků <josef@macku.cz>
2020-11-06 20:29:51 +0100

    Home controller

---
+++ App/Controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+class Home extends \Core\Controller {
+    public function indexAction() {
+        View::renderTemplate('index.html', []);
+    }
+}
