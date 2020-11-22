62b3847b2232bdc7cffec12ddbdefd47302bc87f
Josef Macků <josef@macku.cz>
2020-11-22 16:31:25 +0100

    More spacing fixes

---
+++ App/test_Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class Config {
+    const DB_HOST = 'sql4.webzdarma.cz';
+    const DB_NAME = 'testingsubbo2914';
+    const DB_USER = 'testingsubbo2914';
+    const DB_PASSWORD = 'eV-D-Uv*)D0cXrIFc%5I';
+    const SHOW_ERRORS = true;
+    const TOKEN_SECRET = 'mostsecrettokenstringeva';
+}
